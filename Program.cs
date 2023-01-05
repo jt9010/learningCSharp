8c81fcec5aa45e76028aeb654c5ef2cbabdc0dce
jt9010 <rekosh.cr@gmail.com>
2023-01-05 21:42:11 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string[] jakiesSamochody = { "Volvo", "BMW", "Ford", "Mazda", "Fiat", "Mercedes" };
+Array.Sort(jakiesSamochody);
+foreach (string i in jakiesSamochody)
+{
+    Console.WriteLine(i);
+}
